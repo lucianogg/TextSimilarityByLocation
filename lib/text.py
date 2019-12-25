092f91c8ed2dfd73661ba0240ab7f4722975a7a4
Luciano García Giordano <lucgarc97@hotmail.com>
2019-12-25 22:46:37 +0100

    Add ability to get entities from a text

---
+++ lib/text.py
@@ -0,0 +1,47 @@
+import http
+import http.client
+import json
+import lib.resource as resource
+
+import urllib.parse
+
+class Text():
+    def __init__(self):
+        self.raw_text = None
+        self.locations = []
+
+    @classmethod
+    def text_from_string(cls, string):
+        text = cls()
+        text.raw_text = string
+        return text
+
+    def get_locations(self):
+        processed_text = urllib.parse.quote(self.raw_text)
+        # -----transform text into something like this: -------- 
+        # processed_text = "First+documented+in+the+13th+century%2C+Berlin+was+the+capital+of+the+Kingdom+of+Prussia+(1701%E2%80%931918)%2C+the+German+Empire+(1871%E2%80%931918)%2C+the+Weimar+Republic+(1919%E2%80%9333)+and+the+Third+Reich+(1933%E2%80%9345).+Berlin+in+the+1920s+was+the+third+largest+municipality+in+the+world.+After+World+War+II%2C+the+city+became+divided+into+East+Berlin+--+the+capital+of+East+Germany+--+and+West+Berlin%2C+a+West+German+exclave+surrounded+by+the+Berlin+Wall+from+1961%E2%80%9389.+Following+German+reunification+in+1990%2C+the+city+regained+its+status+as+the+capital+of+Germany%2C+hosting+147+foreign+embassies."
+
+        confidence_threshold = 0.8
+
+        query = "/en/annotate?text=" + processed_text + "&confidence=" + str(confidence_threshold) + "&support=0&spotter=Default&disambiguator=Default&policy=whitelist&types=&sparql="
+
+        conn = http.client.HTTPSConnection("api.dbpedia-spotlight.org", 443)
+        conn.request('GET', query, headers={
+            'Accept': 'application/json'
+        })
+        r1 = conn.getresponse()
+        retrieved_text = r1.read()
+        retrieved_json = json.loads(retrieved_text)
+        conn.close()
+
+        # print(json.dumps(retrieved_json, indent=4))
+
+        resources_detected = [resource.Resource.resource_from_dbpedia_spotlight_annotation(res) for res in retrieved_json['Resources']]
+
+        locations_detected = list(filter(lambda x: x[0] is not None, [res.get_location() for res in resources_detected]))
+        print(locations_detected)
+        return locations_detected
+
+    def get_main_location(self, tolerance_in_km=2000):
+        # Do the clustering stuff to get the "prominent location"
+        pass
